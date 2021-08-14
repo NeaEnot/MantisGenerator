90f8622442ebb1a8ef2ec8d138469169ece51c05
NeaEnot <oleg.kozloss@yandex.ru>
2021-08-15 00:57:27 +0400

    Первоначальное определение задач Выводить узлы Возможность создавать узлы Возможность для каждого узла создавать дочерние узлы Снятие выделения

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MantisGenerator.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,48 @@ namespace MantisGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Node root;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            root = new Node();
+            root.Children.Add(new Node { Name = "Первыф" });
+            root.Children.Add(new Node { Name = "Фторой" });
+            root.Children[0].Children.Add(new Node { Name = "Трефий" });
+            root.Children[0].Children.Add(new Node { Name = "Четфёрфый" });
+            root.Children[1].Children.Add(new Node { Name = "Пяфый" });
+            root.Children[0].Children[0].Children.Add(new Node { Name = "Фестой" });
+        }
+
+        private void treeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            treeView.ItemsSource = null;
+            treeView.Items.Clear();
+            treeView.ItemsSource = root.Children;
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Node selected = (Node)treeView.SelectedItem ?? root;
+
+            NodeWindow nodeWindow = new NodeWindow();
+            if (nodeWindow.ShowDialog() == true)
+            {
+                selected.Children.Add(nodeWindow.Node);
+                LoadData();
+            }
+        }
+
+        private void treeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LoadData();
         }
     }
 }
---
+++ NodeWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MantisGenerator.Models;
+using System.Windows;
+
+namespace MantisGenerator
+{
+    /// <summary>
+    /// Логика взаимодействия для NodeWindow.xaml
+    /// </summary>
+    public partial class NodeWindow : Window
+    {
+        public Node Node { get; private set; }
+
+        public NodeWindow()
+        {
+            InitializeComponent();
+            Node = null;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textTitle.Text))
+            {
+                Node = new Node { Name = textTitle.Text };
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Имя узла пустое", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
